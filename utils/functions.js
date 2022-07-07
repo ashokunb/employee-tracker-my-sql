8b0ed7b4d8026330dcea828a6d75ff14cac40d3b
Ambrose Shokunbi <shokunbitobi@gmail.com>
2022-07-06 19:00:13 -0500

    created additional functions

--- utils/functions.js
+++ utils/functions.js
@@ -5,14 +5,49 @@ inquirer = require("inquirer");
 //functions to run the given command
 function viewAllDepartments() {
   //sql = `SELECT * FROM departments`;
-  db.query(`SELECT * FROM departments`, (err, rows) => { console.table(rows) });
-}
+  db.query(`SELECT * FROM departments`, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+};
+
 function viewAllRoles() {
-  const sql = `SELECT roles.*, departments.name AS departments_Name FROM roles LEFT JOIN departments ON roles.departments_id=departments.id`;
-  db.query(sql, (err, rows) => { console.table(rows) });
+  const sql = `SELECT roles.*, 
+  departments.name AS departments_Name FROM roles LEFT JOIN departments 
+  ON roles.departments_id=departments.id`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+};
+function viewAllEmployees() {
+  const sql1 = `SELECT * FROM employees`;
+  db.query(sql1, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+};
+function addDepartments() {
+  
 }
-function viewAllEmployees() { }
-function addDepartments() { console.log('hello') }
 function addRole() { console.log('hello') }
 function addEmployee() { console.log('hello') }
 function updEmployee() { console.log('hello') }
